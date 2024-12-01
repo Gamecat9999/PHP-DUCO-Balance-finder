b3e79d042da597df5427ff03fce3fe19f179fad1
Gamecat999 <justjames_b@outlook.com>
2024-11-30 17:04:47 -0700

    Add files via upload

---
+++ ducobalfinder.php
@@ -0,0 +1,32 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
+    <h2>Account Balance</h2>
+    <p id="faucet balance">
+        <?php
+            $url = 'https://server.duinocoin.com/balances/katfaucet';
+            $json = @file_get_contents($url);
+            if ($json === FALSE) {
+                echo 'Error fetching balance';
+            } else {
+                $data = json_decode($json, true);
+                if (json_last_error() !== JSON_ERROR_NONE) {
+                    echo 'Error decoding JSON: ' . json_last_error_msg();
+                } else {
+                    if (isset($data['result']['balance'])) {
+                        $balance = $data['result']['balance'];
+                        echo "Balance: $balance DUCO";
+                    } else {
+                        echo 'Error: Balance not found in the response';
+                    }
+                }
+            }
+        ?>
+    </p>
+</body>
+</html>
